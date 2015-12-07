6f76b7dec3478bb36501acc0c2ed6b7f1bff26e6
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2015-12-07 13:09:18 -0700

    This was an aliased commit.

---
+++ jungle_beat.rb
@@ -0,0 +1,35 @@
+class JungleBeat
+
+  def initialize
+    @link = false
+  end
+
+  def link?
+    @link
+  end
+
+  def append
+  end
+
+  def prepend
+  end
+
+  def insert
+  end
+
+  def includes?
+  end
+
+  def pop
+  end
+
+  def count
+  end
+
+  def find
+  end
+
+  def all
+  end
+
+end
